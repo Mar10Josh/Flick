060721175663c645b4fc0fb75cfc82bad4330fea
Josiah <josiahreybacsal@gmail.com>
2022-02-24 18:24:56 -1000

    again

--- main.py
+++ main.py
@@ -35,18 +35,19 @@ with open(tfe(args == [], "app.fk", ' '.join(args))) as f:
    if (line.startsWith("wait(")):
       # Wait for anything
       newline = line
-     newline = newline.replace("\\n", "\n")
-     newline = newline.replace("\\\\", "\\")
-     newline = newline.replace("\\escape|space", "")
-     start = newline.find("(") + 1
-     end = newline.find(")")
-     args = newline[start:end].split(',')
-     if (not args == []):
+      newline = newline.replace("\\n", "\n")
+      newline = newline.replace("\\\\", "\\")
+      newline = newline.replace("\\escape|space", "")
+      start = newline.find("(") + 1
+      end = newline.find(")")
+      args = newline[start:end].split(',')
+      if (not args == []):
         if (args[0] == "seconds" and len(args) == 3):
            print(args[2])
            time.sleep(args[1])
-        else if (args[0] == "enter" and len(args) == 2):
-           input(args[1])
         else:
-           print(newline + " does not have " + tfe(args[0] == "enter", "2","3") + " args! Usage: wait(enter/seconds, prompt(enter)/seconds(seconds),prompt(seconds))")
+           if (args[0] == "enter" and len(args) == 2):
+             input(args[1])
+           else:
+             print(newline + " does not have " + tfe(args[0] == "enter", "2","3") + " args! Usage: wait(enter/seconds, prompt(enter)/seconds(seconds),prompt(seconds))")
            
