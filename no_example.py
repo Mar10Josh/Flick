d62a0bf5e8851639baa575352202a78183649608
Josiah <josiahreybacsal@gmail.com>
2022-02-25 15:25:43 -1000

    ok

--- no_example.py
+++ no_example.py
@@ -2,7 +2,6 @@
 #GUI is not supported.
 
 import sys
-import time
 
 args = sys.argv
 
@@ -31,7 +30,7 @@ if not args == []:
       start = newline.find("(") + 1
       end = newline.find(")")
       print(newline[start:end])
-    if (line.startswith("wait(")):
+    if (line.startsWith("wait(")):
       # Wait for anything
       newline = line
       newline = newline.replace("\\n", "\n")
@@ -44,11 +43,11 @@ if not args == []:
          if (args[0] == "seconds" and len(args) == 3):
             print(args[2])
             time.sleep(args[1])
-         else: 
+         else:
             if (args[0] == "enter" and len(args) == 2):
-              input(args[1])
+             input(args[1])
             else:
-              print(newline + " does not have " + tfe(args[0] == "enter", "2","3") + " args! Usage: wait(enter/seconds, prompt(enter)/seconds(seconds),prompt(seconds))")
+             print(newline + " does not have " + tfe(args[0] == "enter", "2","3") + " args! Usage: wait(enter/seconds, prompt(enter)/seconds(seconds),prompt(seconds))")
            
 
 
